96d06ed0db2d66bf93f0ed6d90814cfddf76aef7
Zion Miller <djzionmiller@gmail.com>
2022-09-26 10:32:38 -0700

    added skeleton for watchlist

--- client/src/components/Watchlist.js
+++ client/src/components/Watchlist.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import { Icon, Label, Menu, Table, Button } from 'semantic-ui-react'
 
 const Watchlist = () => {
   return (
-    <div>Watchlist</div>
+    <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Ticker</Table.HeaderCell>
+        <Table.HeaderCell>Price</Table.HeaderCell>
+        <Table.HeaderCell>Change</Table.HeaderCell>
+        <Table.HeaderCell>Cash</Table.HeaderCell>
+        <Table.HeaderCell>Burn</Table.HeaderCell>
+        <Table.HeaderCell>Catalyst</Table.HeaderCell>
+        <Table.HeaderCell>My Notes</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>
+        <Icon name='delete'></Icon>
+          Ticker here
+        </Table.Cell>
+        <Table.Cell>Price here</Table.Cell>
+        <Table.Cell>Change % from open</Table.Cell>
+        <Table.Cell>Cash</Table.Cell>
+        <Table.Cell>Burn</Table.Cell>
+        <Table.Cell>Catalyst</Table.Cell>
+        <Table.Cell>
+          My Notes
+          <Icon name='expand arrows alternate'></Icon>
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row>
+      <Table.Cell>
+      <Icon name='delete'></Icon>
+        Ticker here
+        </Table.Cell>
+        <Table.Cell>Price here</Table.Cell>
+        <Table.Cell>Change % from open</Table.Cell>
+        <Table.Cell>Cash</Table.Cell>
+        <Table.Cell>Burn</Table.Cell>
+        <Table.Cell>Catalyst</Table.Cell>
+        <Table.Cell>
+          My Notes
+        <Icon name='expand arrows alternate'></Icon>
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row>
+      <Table.Cell>
+      <Icon name='delete'></Icon>
+        Ticker here
+        </Table.Cell>
+        <Table.Cell>Price here</Table.Cell>
+        <Table.Cell>Change % from open</Table.Cell>
+        <Table.Cell>Cash</Table.Cell>
+        <Table.Cell>Burn</Table.Cell>
+        <Table.Cell>Catalyst</Table.Cell>
+        <Table.Cell>My Notes
+        <Icon name='expand arrows alternate'></Icon>
+        </Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
   )
 }
 
