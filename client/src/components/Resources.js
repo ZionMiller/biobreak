b29b332e0cd66d0a06368a0e06701cc7f3eafb47
Zion Miller <djzionmiller@gmail.com>
2022-09-26 10:51:17 -0700

    updated resources route

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -1,15 +1,38 @@
 import React from 'react'
+import Faq from 'react-faq-component';
 import { Embed } from 'semantic-ui-react'
 
 const Resources = () => {
+
+    const data = {
+        title: "FAQ (How it works)",
+        rows: [
+          {
+            title: "Lorem ipsum dolor sit amet,",
+            content: "Lorem ipsum dolor sit amet, consectetur "
+          },
+          {
+            title: "Nunc maximus, magna at ultricies elementum",
+            content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
+          },
+          {
+            title: "Curabitur laoreet, mauris vel blandit fringilla",
+            content: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+          },
+          {
+            title: "What is the package version",
+            content: "v1.0.5"
+          }]
+      }
+
   return (
     <div>
-    <h1>Explainer video coming soon</h1>
-        <Embed
+        <Faq data={data} />
+        {/* <Embed
         icon='right circle arrow'
         placeholder='/images/image-16by9.png'
         url='https://www.myfav.es/jack'
-    />
+    /> */}
   </div>
   )
 }
